12c381d051c0ba17187bb1237cffe9d533d7349e
zengqinglin <328318821@qq.com>
2019-02-15 11:27:12 +0800

    版本:0.0.18

---
+++ zp02yx_xzmbhTMHigh/Classes/zp02yx_xzmbhTMHigh.framework/Headers/zp02yx_xzmbhTMHigh.h
@@ -0,0 +1,21 @@
+//
+//  zp02yx_xzmbhTMHigh.h
+//  zp02yx_xzmbhTMHigh
+//
+//  Created by Mac on 2018/12/25.
+//  Copyright © 2018年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for zp02yx_xzmbhTMHigh.
+FOUNDATION_EXPORT double zp02yx_xzmbhTMHighVersionNumber;
+
+//! Project version string for zp02yx_xzmbhTMHigh.
+FOUNDATION_EXPORT const unsigned char zp02yx_xzmbhTMHighVersionString[];
+
+
+// In this header, you should import all the public headers of your framework using statements like #import <zp02yx_xzmbhTMHigh/PublicHeader.h>
+
+
+
